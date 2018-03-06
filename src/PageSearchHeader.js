4999a5ae3c6e12353d93f5b5594b5ac96bed7075
Jean Dessane <jean.dessane@gmail.com>
2018-03-06 22:37:23 +0000

    tamer

--- src/PageSearchHeader.js
+++ src/PageSearchHeader.js
@@ -24,8 +24,8 @@ export default class PageSearchHeader extends Component {
   render() {
     return (
       <Header iosBarStyle="light-content" androidStatusBarColor="#4b28b7" searchBar rounded style={{ backgroundColor: '#6136e8' }}>
-        <Button onPressIn={this.props.onBack} transparent style={{ width: 30, paddingVertical: 0 }} >
-          <Icon name="arrow-back" style={{ color: '#ffffff' }} />
+        <Button onPressIn={this.props.onBack} transparent style={{ width: 30 }} >
+          <Icon name="arrow-back" style={{ color: '#ffffff', margin: 0, padding: 0 }} />
         </Button>
         <Item style={{ backgroundColor: 'white' }}>
           {Platform.OS === 'android' && <Button onPressIn={this.props.onBack} transparent style={{ paddingBottom: 10 }}>
