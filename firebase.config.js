55bc4d48f566d14f20691b1115035e707751acc5
Jean Dessane <jean.dessane@gmail.com>
2018-03-21 08:58:16 +0000

    conf

--- firebase.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyCvRLFX4OdB0s0Hx5Tyh9RbfMiSDDqskrc',
-  authDomain: 'nikki-8d0a5.firebaseapp.com',
-  databaseURL: 'https://nikki-8d0a5.firebaseio.com',
-  projectId: 'nikki-8d0a5',
-  storageBucket: 'nikki-8d0a5.appspot.com',
-  messagingSenderId: '916258383936',
-};
